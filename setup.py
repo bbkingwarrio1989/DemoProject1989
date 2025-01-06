b6261fa80656c70bc912b8a32eea26225bf8bef9
Balachander B <bchander21@gmail..com>
2025-01-06 13:43:03 +0400

    Code second

--- setup.py
+++ setup.py
@@ -1,2 +1,22 @@
-from setuptools import find_packages
-from typing import Text
+from setuptools import find_packages ,setup
+from typing import List
+HYPEN_E_DOT = '-e.'
+
+def get_requirements(filepath : str) ->List[str]:
+    requirements =[]
+    with open(filepath) as file_Obj:
+        requirements = file_Obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+    if(HYPEN_E_DOT) in requirements :
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='DemoProject1989',
+    version='0.0.1',
+    author='bbkingwarrio1989',
+    author_email='bchander21@gmail.com',
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt')
+)
